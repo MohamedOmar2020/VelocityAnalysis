2189b95b6d0d50fdb2f7cb9aa978b6c25314ed2f
Mohamed Omar <mohamed222ahmed222@gmail.com>
2020-12-12 19:13:22 -0500

    updated VelocityR script

--- Code/VelocityR.R
+++ Code/VelocityR.R
@@ -7,17 +7,30 @@ library(loomR)
 library(SCopeLoomR)
 
 ## Load loom data
-ldat <- open_loom("~/Documents/Research/Projects/Velocity/Loom_Combined.loom", mode = "r+")
+ldat <- connect("~/Documents/Research/Projects/Velocity/Data/BigLoomFiltered.loom", mode = "r+", skip.validate = T)
 
-##Normalize and cluster cells using pagoda2
-# Using spliced expression matrix as input to pagoda2.
-emat <- ldat$spliced
-# this is where one woudl do some filtering
-emat <- emat[,colSums(emat) >= 1000]
+fit.quantile <- 0.1
 
+## pull out spliced and unspliced matrices from AnnData
+emat <- as.matrix(t(ldat$layers['spliced']))
+nmat <- as.matrix(t(adata$layers['unspliced']))
+cells <- adata$obs_names$values
+genes <- adata$var_names$values
+colnames(emat) <- colnames(nmat) <- cells
+rownames(emat) <- rownames(nmat) <- genes
+
+## pull out PCA 
+pcs <- adata$obsm['X_pca']
+rownames(pcs) <- cells
+cell.dist <- as.dist(1-cor(t(pcs))) ## cell distance in PC space
+
+## filter genes
+gexp1 <- log10(rowSums(emat)+1)
+gexp2 <- log10(rowSums(nmat)+1)
+#plot(gexp1, gexp2)
+good.genes <- genes[gexp1 > 2 & gexp2 > 1]
+
+
+
+## Close loom connection
 ldat$close_all()
-
-gr_all <- merge(IntestineSeurat, as.Seurat(ldat)[, colnames(IntestineSeurat)])
-
-
-
